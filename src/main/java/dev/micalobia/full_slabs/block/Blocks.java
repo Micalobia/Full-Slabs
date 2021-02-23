5013c8ac37fead7ee06d7362796bd8ed30c3b3da
Micah <micalobia2@gmail.com>
2021-02-23 12:45:19 -0700

    Fixed crash on incorrect load order

--- src/main/java/dev/micalobia/full_slabs/block/Blocks.java
+++ src/main/java/dev/micalobia/full_slabs/block/Blocks.java
@@ -4,6 +4,7 @@ import dev.micalobia.full_slabs.block.entity.FullSlabBlockEntity;
 import dev.micalobia.full_slabs.util.Helper;
 import dev.micalobia.full_slabs.util.LinkedSlabs;
 import dev.micalobia.full_slabs.util.TiltedSlabs;
+import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.SlabBlock;
@@ -19,6 +20,7 @@ public class Blocks {
 
 	static {
 		Registry.BLOCK.forEach(Blocks::generateVerticalPair);
+		RegistryEntryAddedCallback.event(Registry.BLOCK).register(Blocks::hookIntoRegister);
 		FULL_SLAB_BLOCK = register("full_slabs:full_slab_block", new FullSlabBlock(FabricBlockSettings.copyOf(net.minecraft.block.Blocks.BEDROCK)));
 		FULL_SLAB_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, "full_slabs:full_slab", BlockEntityType.Builder.create(FullSlabBlockEntity::new, Blocks.FULL_SLAB_BLOCK).build(null));
 	}
@@ -27,12 +29,19 @@ public class Blocks {
 		return Registry.register(Registry.BLOCK, id, block);
 	}
 
-	private static void generateVerticalPair(Block block) {
+	private static void hookIntoRegister(int i, Identifier identifier, Block block) {
+		generateVerticalPair(identifier, block);
+	}
+
+	private static void generateVerticalPair(Identifier base, Block block) {
 		if (!(block instanceof SlabBlock)) return;
-		Identifier base = Helper.fetchId(block);
 		boolean tilted = TiltedSlabs.contains(base);
 		Identifier vertical = new Identifier("full_slabs", base.getNamespace() + "_" + base.getPath() + "_vertical");
 		Block verticalSlab = register(vertical.toString(), new VerticalSlabBlock(FabricBlockSettings.copyOf(block), tilted));
 		LinkedSlabs.link(block, verticalSlab);
+	}
+
+	private static void generateVerticalPair(Block block) {
+		generateVerticalPair(Helper.fetchId(block), block);
 	}
 }
