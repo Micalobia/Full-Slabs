a96cd9c8dc276c3671e8557fd0af6d3229e9f3a5
Micalobia <micalobia2@gmail.com>
2021-09-27 11:11:58 -0700

    Compatibility (#31)
    * Removed Redirect from WorldChunkMixin
    
    * Removed Redirect from EntityMixin
    
    * Removed Redirect from LivingEntityMixin
    
    * Removed Redirects from ModelLoaderMixin
    
    * Removed Redirect from BlockRenderManagerMixin
    
    * Removed Redirect from ParticleManagerMixin

--- src/main/java/dev/micalobia/full_slabs/mixin/client/render/block/BlockRenderManagerMixin.java
+++ src/main/java/dev/micalobia/full_slabs/mixin/client/render/block/BlockRenderManagerMixin.java
@@ -12,9 +12,7 @@ import net.minecraft.block.SlabBlock;
 import net.minecraft.block.enums.SlabType;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.block.BlockModels;
 import net.minecraft.client.render.block.BlockRenderManager;
-import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
@@ -23,26 +21,40 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.BlockRenderView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(BlockRenderManager.class)
 public class BlockRenderManagerMixin {
+	private RenderAttachedBlockView view;
+	private BlockPos pos;
+
+	@Inject(method = "renderDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/BlockModels;getModel(Lnet/minecraft/block/BlockState;)Lnet/minecraft/client/render/model/BakedModel;"))
+	private void skimSlabDamageInfo(BlockState state, BlockPos pos, BlockRenderView world, MatrixStack matrix, VertexConsumer vertexConsumer, CallbackInfo ci) {
+		boolean isFullSlab = state.isOf(FullSlabsMod.FULL_SLAB_BLOCK);
+		if(isFullSlab || state.getBlock() instanceof SlabBlock) {
+			this.pos = pos;
+			if(isFullSlab)
+				this.view = (RenderAttachedBlockView) world;
+		}
+	}
+
 	@SuppressWarnings("unchecked")
-	@Redirect(method = "renderDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/BlockModels;getModel(Lnet/minecraft/block/BlockState;)Lnet/minecraft/client/render/model/BakedModel;"))
-	private BakedModel interceptSlabDamageRender(BlockModels blockModels, BlockState state_, BlockState state, BlockPos pos, BlockRenderView world, MatrixStack matrix, VertexConsumer vertexConsumer) {
+	@ModifyArg(method = "renderDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/BlockModels;getModel(Lnet/minecraft/block/BlockState;)Lnet/minecraft/client/render/model/BakedModel;"), index = 0)
+	private BlockState changeSlabDamageRender(BlockState state) {
 		if(state.isOf(FullSlabsMod.FULL_SLAB_BLOCK)) {
-			RenderAttachedBlockView view = (RenderAttachedBlockView) world;
 			Axis axis = state.get(Properties.AXIS);
 			assert MinecraftClient.getInstance().crosshairTarget != null;
 			Vec3d hit = MinecraftClient.getInstance().crosshairTarget.getPos();
-			Pair<Block, Block> pair = (Pair<Block, Block>) view.getBlockEntityRenderAttachment(pos);
-			return blockModels.getModel(Utility.getSlabState(pair, axis, hit, pos));
+			Pair<Block, Block> pair = (Pair<Block, Block>) this.view.getBlockEntityRenderAttachment(this.pos);
+			return Utility.getSlabState(pair, axis, hit, this.pos);
 		} else if(state.getBlock() instanceof SlabBlock && state.get(SlabBlock.TYPE) == SlabType.DOUBLE) {
 			assert MinecraftClient.getInstance().crosshairTarget != null;
 			Vec3d hit = MinecraftClient.getInstance().crosshairTarget.getPos();
-			boolean positive = Utility.isPositive(state.get(Properties.AXIS), hit, pos);
-			return blockModels.getModel(state.with(SlabBlock.TYPE, positive ? SlabType.TOP : SlabType.BOTTOM));
-		} else return blockModels.getModel(state);
+			boolean positive = Utility.isPositive(state.get(Properties.AXIS), hit, this.pos);
+			return state.with(SlabBlock.TYPE, positive ? SlabType.TOP : SlabType.BOTTOM);
+		} else return state;
 	}
 }
--- src/main/java/dev/micalobia/full_slabs/mixin/entity/EntityMixin.java
+++ src/main/java/dev/micalobia/full_slabs/mixin/entity/EntityMixin.java
@@ -5,15 +5,15 @@ import dev.micalobia.full_slabs.block.entity.FullSlabBlockEntity;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.particle.BlockStateParticleEffect;
+import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleType;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
@@ -21,27 +21,18 @@ public abstract class EntityMixin {
 	public World world;
 
 	@Shadow
-	public abstract double getX();
-
-	@Shadow
-	public abstract double getY();
-
-	@Shadow
-	public abstract double getZ();
-
-	@Shadow
 	public abstract Vec3d getPos();
 
-	@Redirect(method = "spawnSprintingParticles", at = @At(value = "NEW", target = "net/minecraft/particle/BlockStateParticleEffect"))
-	private BlockStateParticleEffect fullSlabSprintParticles(ParticleType<BlockStateParticleEffect> type, BlockState state) {
+	@ModifyArg(method = "spawnSprintingParticles", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"), index = 0)
+	private ParticleEffect fullSlabSprintParticles(ParticleEffect particleEffect) {
+		BlockStateParticleEffect effect = (BlockStateParticleEffect) particleEffect;
+		BlockState state = effect.getBlockState();
+		ParticleType<BlockStateParticleEffect> type = effect.getType();
 		if(!state.isOf(FullSlabsMod.FULL_SLAB_BLOCK))
 			return new BlockStateParticleEffect(type, state);
-		int x = MathHelper.floor(getX());
-		int y = MathHelper.floor(getY() - 0.2d);
-		int z = MathHelper.floor(getZ());
-		BlockPos pos = new BlockPos(x, y, z);
-		FullSlabBlockEntity entity = (FullSlabBlockEntity) world.getBlockEntity(pos);
 		Vec3d hit = getPos().subtract(0d, 0.2d, 0d);
+		BlockPos pos = new BlockPos(hit);
+		FullSlabBlockEntity entity = (FullSlabBlockEntity) world.getBlockEntity(pos);
 		assert entity != null;
 		BlockState particleState = entity.getSlabState(hit);
 		return new BlockStateParticleEffect(type, particleState);
