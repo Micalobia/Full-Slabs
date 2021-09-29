c6bc2c6a1bceb04fe4659f68c7602f1d9410e698
Micah <micalobia2@gmail.com>
2021-09-29 10:37:06 -0700

    Compatibility for Carpet

--- src/main/java/dev/micalobia/full_slabs/mixin/world/WorldChunkMixin.java
+++ src/main/java/dev/micalobia/full_slabs/mixin/world/WorldChunkMixin.java
@@ -1,17 +1,35 @@
 package dev.micalobia.full_slabs.mixin.world;
 
 import com.mojang.datafixers.util.Pair;
+import dev.micalobia.full_slabs.FullSlabsMod;
 import dev.micalobia.full_slabs.block.entity.FullSlabBlockEntity;
 import dev.micalobia.full_slabs.util.Utility;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.world.chunk.WorldChunk;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(WorldChunk.class)
 public abstract class WorldChunkMixin {
+	@Shadow
+	public abstract void setBlockEntity(BlockEntity blockEntity);
+
+	// For compatibility with Carpet, doesn't make them movable but does prevent them from defaulting to STONE-STONE
+	@ModifyVariable(method = "setBlockState", at = @At(shift = Shift.BEFORE, value = "JUMP", opcode = Opcodes.IFNONNULL))
+	private BlockEntity changeFullSlabBlockEntityCarpet(BlockEntity entity) {
+		if(entity instanceof FullSlabBlockEntity) {
+			Pair<Block, Block> pair = Utility.getGhostPair();
+			BlockEntity blockEntity = new FullSlabBlockEntity(entity.getPos(), entity.getCachedState(), pair.getFirst(), pair.getSecond());
+			this.setBlockEntity(blockEntity);
+			return blockEntity;
+		} else return entity;
+	}
+
 	@ModifyVariable(method = "setBlockState", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/BlockEntityProvider;createBlockEntity(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Lnet/minecraft/block/entity/BlockEntity;"))
 	private BlockEntity changeFullSlabEntity(BlockEntity entity) {
 		if(entity instanceof FullSlabBlockEntity) {
