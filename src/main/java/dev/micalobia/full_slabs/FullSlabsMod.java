7d8e871daa1a075ee373a79f6ba60837bb0d50c3
Micah <micalobia2@gmail.com>
2021-10-02 04:35:25 -0700

    Attempt to increase load time

--- src/main/java/dev/micalobia/full_slabs/FullSlabsMod.java
+++ src/main/java/dev/micalobia/full_slabs/FullSlabsMod.java
@@ -9,6 +9,7 @@ import dev.micalobia.full_slabs.util.Utility;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
+import net.fabricmc.fabric.api.event.registry.RegistryIdRemapCallback;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.AbstractBlock.Settings;
 import net.minecraft.block.Block;
@@ -47,10 +48,10 @@ public class FullSlabsMod implements ModInitializer {
 		FULL_SLAB_BLOCK = Registry.register(Registry.BLOCK, id("full_slab_block"), new FullSlabBlock(Settings.copy(Blocks.BEDROCK)));
 		FULL_SLAB_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, id("full_slab"), FabricBlockEntityTypeBuilder.create(FullSlabBlockEntity::new, FULL_SLAB_BLOCK).build());
 
+		RegistryIdRemapCallback.event(Registry.BLOCK).register((state -> StateRefresher.INSTANCE.reorderBlockStates()));
 		Utility.injectBlockProperty(SlabBlock.class, Properties.AXIS, Axis.Y);
 		RegistryEntryAddedCallback.event(Registry.BLOCK).register(((rawId, id, object) -> {
 			if (object instanceof SlabBlock) Utility.injectBlockProperty(SlabBlock.class, Properties.AXIS, Axis.Y);
-			StateRefresher.INSTANCE.reorderBlockStates();
 		}));
 		StateRefresher.INSTANCE.reorderBlockStates();
 	}
