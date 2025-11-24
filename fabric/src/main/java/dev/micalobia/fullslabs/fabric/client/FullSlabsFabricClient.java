05fb5605696dad2e69a321ed164d6442b3f5031b
Micah <micalobia2@gmail.com>
2025-11-23 23:00:46 -0800

    Fix for Iris + Mo Glass
    Issue #93
    This was a load order issue, I delegated some render layer calls to solve it.

--- fabric/src/main/java/dev/micalobia/fullslabs/fabric/client/FullSlabsFabricClient.java
+++ fabric/src/main/java/dev/micalobia/fullslabs/fabric/client/FullSlabsFabricClient.java
@@ -11,16 +11,28 @@ import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.core.registries.BuiltInRegistries;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class FullSlabsFabricClient implements ClientModInitializer {
+    private static boolean clientStarted = false;
+    private static final List<VerticalSlabBlock> pending = new ArrayList<>();
+
     @Override
     public void onInitializeClient() {
         FullSlabsClient.init();
         ModelLoadingPlugin.register(new VerticalModelLoadingPlugin());
-        ClientLifecycleEvents.CLIENT_STARTED.register(client -> VerticalSlabBlock.MAP_VIEW.values().forEach(FullSlabsFabricClient::renderLayer));
-        // I'm not sure if this is necessary, but just in case
+        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            clientStarted = true;
+            VerticalSlabBlock.MAP_VIEW.values().forEach(FullSlabsFabricClient::renderLayer);
+            pending.forEach(FullSlabsFabricClient::renderLayer);
+            pending.clear();
+        });
+        // I'm not sure if this is necessary, but it's difficult to test load order
         RegistryEntryAddedCallback.event(BuiltInRegistries.BLOCK).register(((i, identifier, block) -> {
             if (!(block instanceof VerticalSlabBlock slab)) return;
-            renderLayer(slab);
+            if (clientStarted) renderLayer(slab);
+            else pending.add(slab);
         }));
         Controls.clientInit();
     }
@@ -29,3 +41,4 @@ public final class FullSlabsFabricClient implements ClientModInitializer {
         BlockRenderLayerMap.putBlock(slab, ItemBlockRenderTypes.getChunkRenderType(slab.parent.defaultBlockState()));
     }
 }
+
